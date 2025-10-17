28e314dc2815e2669e0535062792f563d29023eb
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-17 18:20:40 +0900

    feat: 유저 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from '@woowacourse/mission-utils';
+import MESSAGES from './message.js';
+
 class App {
-  async run() {}
+  async run() {
+    const INPUT_STRING = await this.userInput(MESSAGES.USER_INPUT);
+  }
+
+  async userInput(content) {
+    return await Console.readLineAsync(content);
+  }
 }
 
 export default App;
