6af1bd332c929a2db4f42669019b7c62cc7ab4dd
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-17 18:17:28 +0900

    feat: 메세지 상수화 추가

---
+++ src/message.js
@@ -0,0 +1,9 @@
+const MESSAGES = Object.freeze({
+  USER_INPUT: '덧셈할 문자열을 입력해 주세요.\n',
+  PRINT_RESULT: '결과 : ',
+  NOT_NUMBER: '[ERROR] 입력하신 문자열 중 숫자가 없습니다.',
+  INVALID_INPUT_FORMAT: '[ERROR] 입력하신 문자열이 구분자와 숫자 이외의 값이 들어있습니다.',
+  EMPTY_STRING: '[ERROR] 입력하신 문자열이 없습니다.',
+});
+
+export default MESSAGES;
