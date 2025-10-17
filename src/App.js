38b081dc5683fb75e7b216000ec310ea58ec275a
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-17 18:39:49 +0900

    feat: 커스텀 구분자 구분

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import { Console } from '@woowacourse/mission-utils';
-import MESSAGES from './message.js';
+import MESSAGES from './constants/message.js';
+import { DEFAULT_SEPARATORS, CUSTOM_SEPARATOR_PATTERN } from './constants/separator.js';
 
 class App {
   #separators;
 
   async run() {
     const INPUT_STRING = await this.userInput(MESSAGES.USER_INPUT);
+    this.setSeparator(INPUT_STRING);
+    this.printResult(this.#separators);
   }
 
   async userInput(content) {
     return await Console.readLineAsync(content);
   }
 
+  async printResult(result) {
+    return await Console.print(MESSAGES.PRINT_RESULT + result);
+  }
+
   setSeparator(inputString) {
-    this.#separators = [',', ':'];
+    this.#separators = DEFAULT_SEPARATORS;
+    const customSeparator = inputString.match(CUSTOM_SEPARATOR_PATTERN)?.[1];
+    if (customSeparator) {
+      this.#separators.push(customSeparator);
+    }
   }
 }
 
