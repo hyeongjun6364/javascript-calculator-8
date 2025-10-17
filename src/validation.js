6723e88fa61615656e54d7a3ac3748cfd6253e4e
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-17 20:19:56 +0900

    feat: 입력 받은 문자열에 숫자가 없을 경우 예외처리

--- src/validation.js
+++ src/validation.js
@@ -2,6 +2,8 @@ import MESSAGES from './constants/message.js';
 
 export const inputValidation = (content) => {
   const IS_EMPTY = content.length === 0;
+  const IS_NOT_INCLUDE_NUMBER = content.split('').every((char) => isNaN(char));
 
   if (IS_EMPTY) throw new Error(MESSAGES.EMPTY_STRING);
+  if (IS_NOT_INCLUDE_NUMBER) throw new Error(MESSAGES.NOT_NUMBER);
 };
