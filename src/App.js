9400b4e7462a99cc1637775623e19b784b734b06
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-17 18:24:46 +0900

    feat: 기본 구분자 지정

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import { Console } from '@woowacourse/mission-utils';
 import MESSAGES from './message.js';
 
 class App {
+  #separators;
+
   async run() {
     const INPUT_STRING = await this.userInput(MESSAGES.USER_INPUT);
   }
 
   async userInput(content) {
     return await Console.readLineAsync(content);
+  }
+
+  setSeparator(inputString) {
+    this.#separators = [',', ':'];
   }
 }
 
