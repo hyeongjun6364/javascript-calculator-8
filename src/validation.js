b7028517c1d4c1d914ee2f66881ef38d7fe68fdd
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-17 20:47:11 +0900

    feat: 구분자, 숫자 이외의 문자열 입력한 경우 예외처리

--- src/validation.js
+++ src/validation.js
@@ -1,9 +1,12 @@
 import MESSAGES from './constants/message.js';
 
-export const inputValidation = (content) => {
-  const IS_EMPTY = content.length === 0;
-  const IS_NOT_INCLUDE_NUMBER = content.split('').every((char) => isNaN(char));
+export const inputValidation = (content, separators) => {
+  const isEmpty = content.length === 0;
+  const isNotIncludeNumber = content.split('').every((char) => isNaN(char));
+  const inValidCharsRegex = new RegExp(`[^0-9${separators.join('')}]`);
+  const hasInvalidChars = inValidCharsRegex.test(content);
 
-  if (IS_EMPTY) throw new Error(MESSAGES.EMPTY_STRING);
-  if (IS_NOT_INCLUDE_NUMBER) throw new Error(MESSAGES.NOT_NUMBER);
+  if (isEmpty) throw new Error(MESSAGES.EMPTY_STRING);
+  if (isNotIncludeNumber) throw new Error(MESSAGES.NOT_NUMBER);
+  if (hasInvalidChars) throw new Error(MESSAGES.INVALID_INPUT_FORMAT);
 };
