863435bc48a5496d4bdcc6671eeac9776b0ff6eb
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-20 18:19:26 +0900

    test: 예외, 성공테스트 케이스 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,5 @@
-import App from "../src/App.js";
-import { MissionUtils } from "@woowacourse/mission-utils";
+import App from '../src/App.js';
+import { MissionUtils } from '@woowacourse/mission-utils';
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -11,18 +11,18 @@ const mockQuestions = (inputs) => {
 };
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
   return logSpy;
 };
 
-describe("문자열 계산기", () => {
-  test("커스텀 구분자 사용", async () => {
-    const inputs = ["//;\\n1"];
+describe('문자열 계산기', () => {
+  test('커스텀 구분자 사용', async () => {
+    const inputs = ['//;\\n1'];
     mockQuestions(inputs);
 
     const logSpy = getLogSpy();
-    const outputs = ["결과 : 1"];
+    const outputs = ['결과 : 1'];
 
     const app = new App();
     await app.run();
@@ -31,13 +31,41 @@ describe("문자열 계산기", () => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
     });
   });
-
-  test("예외 테스트", async () => {
-    const inputs = ["-1,2,3"];
+  test('예외 테스트', async () => {
+    const inputs = ['-1,2,3'];
     mockQuestions(inputs);
 
     const app = new App();
 
-    await expect(app.run()).rejects.toThrow("[ERROR]");
+    await expect(app.run()).rejects.toThrow('[ERROR]');
+  });
+
+  test.each([
+    ['//;\n;', '[ERROR]'],
+    ['aaa', '[ERROR]'],
+    ['1a2,c', '[ERROR]'],
+  ])('입력: %s에 대한 예외테스트', async (input, expected) => {
+    mockQuestions([input]);
+
+    const app = new App();
+    await expect(app.run()).rejects.toThrow(expected);
+  });
+
+  test.each([
+    ['1,2,3', 6],
+    ['1:2:3', 6],
+    ['1,2:3', 6],
+    ['5', 5],
+    ['//;\\n4;2,3', 9],
+    ['//ab\\n1ab2,3', 6],
+  ])('입력 "%s"에 대한 정상테스트', async (input, expected) => {
+    const inputs = [input];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const app = new App();
+    await app.run();
+
+    expect(logSpy).toHaveBeenCalledWith(`결과 : ${expected}`);
   });
 });
